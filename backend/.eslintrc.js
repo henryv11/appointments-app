4ba8063ab4958045bd432e587728123ae848cc91
Henry <henry@coolbet.com>
2020-09-29 16:11:20 +0300

    p

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 2020,
-
         sourceType: 'module',
     },
     extends: ['plugin:@typescript-eslint/recommended'],
