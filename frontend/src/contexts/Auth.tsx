d8be82fc5d17fe49d86bee2f6f6648a3f2428879
henry viirmäe <heviir@ttu.ee>
2020-10-09 10:04:18 +0300

    ...

--- frontend/src/contexts/Auth.tsx
+++ frontend/src/contexts/Auth.tsx
@@ -1,6 +1,7 @@
+import { User } from '../@types/user';
 import { buildContext } from '../higher-order/Build';
 
-type AuthState = { isAuthenticated: boolean; token?: string; user?: { id: number; username: string } };
+type AuthState = { isAuthenticated: boolean; token?: string; user?: Pick<User, 'id' | 'username'> };
 
 type AuthDispatch = { type: string };
 
