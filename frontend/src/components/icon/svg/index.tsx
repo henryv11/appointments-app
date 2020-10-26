f1ba5373bde6467a490e030c79be0dc7da669863
Henry <henry@coolbet.com>
2020-10-26 13:11:04 +0200

    added new icons

--- frontend/src/components/icon/svg/index.tsx
+++ frontend/src/components/icon/svg/index.tsx
@@ -1,38 +1,55 @@
 import React from 'react';
-import test from './test';
-import test2 from './test2';
+import chevronLeft from './chevron-left';
+import chevronRight from './chevron-right';
+import code from './code';
+import expandLess from './expand-less';
+import expandMore from './expand-more';
 
 const icons = asIconDescriptors({
-  test,
-  test2,
+  chevronLeft,
+  chevronRight,
+  code,
+  expandMore,
+  expandLess,
 });
 
-export default function SvgIcon<T extends IconName>({ name, width, height, size, ...iconProps }: SvgIconProps<T>) {
-  const { svgProps, Icon } = icons[name];
-  width = width ?? size ?? 64;
-  height = height ?? size ?? 64;
+export default function SvgIcon<T extends IconName>({ icon, width, height, size, ...iconProps }: SvgIconProps<T>) {
+  const { svgProps, Icon } = icons[icon];
+  width = width ?? size ?? 32;
+  height = height ?? size ?? 32;
   return (
-    <svg aria-labelledby='title' width={`${width}px`} height={`${height}px`} {...svgProps}>
-      <title id='title'>{`${name} Icon`}</title>
-      <Icon {...iconProps} />
+    <svg
+      aria-labelledby='icon-title'
+      xmlns='http://www.w3.org/2000/svg'
+      width={`${width}px`}
+      height={`${height}px`}
+      {...svgProps}
+    >
+      <title id='icon-title'>{`${icon} Icon`}</title>
+      <Icon {...(iconProps as never)} />
     </svg>
   );
 }
 
-function asIconDescriptors<T extends Record<string, IconDescriptor<Props>>, Props>(map: { [K in keyof T]: T[K] }) {
+function asIconDescriptors<T extends Record<string, IconDescriptor<Props>>, Props extends Record<string, unknown>>(
+  map: { [K in keyof T]: T[K] },
+) {
   return map;
+}
+interface IconDescriptor<T extends Record<string, unknown>> {
+  svgProps: React.SVGProps<SVGSVGElement>;
+  Icon: (props?: T) => JSX.Element;
 }
 
 type IconName = keyof typeof icons;
 
-interface IconDescriptor<T> {
-  svgProps: React.SVGProps<SVGSVGElement>;
-  Icon: (props: T) => JSX.Element;
-}
+type InferIconProps<T extends IconName> = Parameters<typeof icons[T]['Icon']>[0] extends undefined
+  ? {}
+  : Parameters<typeof icons[T]['Icon']>[0];
 
 type SvgIconProps<T extends IconName> = {
-  name: T;
+  icon: T;
   width?: number;
   height?: number;
   size?: number;
-} & Parameters<typeof icons[T]['Icon']>[0];
+} & InferIconProps<T>;
